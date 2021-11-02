7d28022f96a3fb9bab72fbb4ad55bc3884de63a3
윤희겸 <dbsgmlrua@naver.com>
2021-11-03 00:02:37 +0900

    migrate to rinkeby

--- nft-front/truffle-config.js
+++ nft-front/truffle-config.js
@@ -1,4 +1,11 @@
 
+require('dotenv').config()
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const mnemonic = process.env.MNEMONIC
+const clientURL = process.env.ETH_CLIENT_URL
+
 module.exports = {
   networks: {
     development: {
@@ -6,11 +13,15 @@ module.exports = {
      port: 7545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
-    linkeby: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 7545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
-    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, clientURL),
+      network_id: 4,       // Rinkeby's id
+      gas: 3000000,
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true  ,   // Skip dry run before migrations? (default: false for public nets )
+      networkCheckTimeout: 10000000,
+    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
